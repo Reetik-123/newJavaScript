4f414bfdc597604d3f771cfc9fa012a4cbf03a32
Reetik <reetikk77@gmail.com>
2026-02-03 21:04:20 +0530

    variables and dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert("hello") // we are usuing nodejs , not browser
+
+console.log(3 
+    + 3) // code readablility should be high
+
+console.log("Reetik")
+
+let name = "Reetik"
+let age = 20
+let isloggedin = false
+
+
+// number => 2 to power 53 
+// bigint
+// string => "" , '' , ``
+// boolean => true/false
+// null => standalone value 
+// undefined => variable value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object (bug)

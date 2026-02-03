3164ad12d0e140b43895e9c1feb1c4a744464207
Reetik <reetikk77@gmail.com>
2026-02-03 22:05:20 +0530

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Reetik"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+let isloggedIn = "Reetik"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Reetik" => true
+// null => false
+// undefined => false
+// " " => true
+// [] => true
+// {} => true
+
+let someNumber = 33
+
+let stringNUmber = String(someNumber)
+console.log(stringNUmber)
+console.log(typeof stringNUmber)

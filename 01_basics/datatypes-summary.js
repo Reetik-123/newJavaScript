d49e6c10b9a4e491521af9c913bb28fe4bf15bcf
Reetik <reetikk77@gmail.com>
2026-02-07 20:49:33 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3456789012345678901234567890n
+
+
+
+// Reference (Non-primitive)
+
+// Object, Array, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Reetik",
+    age: 20,
+} 
+
+const muFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof outsideTemp); 
